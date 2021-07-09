db742ccbec30095dc35556a2b8242c1084bffae8
notd5a-alt <ahmadabushawar21@gmail.com>
2021-07-09 14:43:27 +0400

    Set Up Bento webpage to match my usecase

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d72';
+const key = 'fb80d89c5ff6e9ce65d2fe7ce4db5e54';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
   // Here you can change your position
   // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-  let latitude = 37.774929;
-  let longitude = -122.419418;
+  let latitude = 25.204849;
+  let longitude = 55.270782;
 
   getWeather(latitude, longitude);
 }
